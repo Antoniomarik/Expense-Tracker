fc8714b8a0ba9e39ef6bcd6825478d4d1e2950cd
Antoniomarik <am46906@oss.unist.hr>
2023-02-28 23:38:36 +0100

    added 1 custom validator and other from dependencies

--- src/main/java/makelli/expensetracker/Controller/ExpenseController.java
+++ src/main/java/makelli/expensetracker/Controller/ExpenseController.java
@@ -4,15 +4,18 @@ import makelli.expensetracker.DTO.ExpenseDTO;
 import makelli.expensetracker.DTO.ExpenseFilterDto;
 import makelli.expensetracker.Entity.Expense;
 import makelli.expensetracker.Service.ExpenseService;
+import makelli.expensetracker.validator.ExpenseValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.List;
@@ -43,8 +46,12 @@ public class ExpenseController {
     }
 
     @PostMapping("/saveorupdateexpense")
-    public String expenseFormPOST(@ModelAttribute("expense")ExpenseDTO expenseDTO) throws ParseException {
+    public String expenseFormPOST(@Valid @ModelAttribute("expense")ExpenseDTO expenseDTO, BindingResult result) throws ParseException {
+        new ExpenseValidator().validate(expenseDTO,result);
 
+        if(result.hasErrors()){
+            return "expense-form";
+        }
         expenseService.saveExpanseDetails(expenseDTO);
 
         return "redirect:/expenses";
--- src/main/java/makelli/expensetracker/DTO/ExpenseDTO.java
+++ src/main/java/makelli/expensetracker/DTO/ExpenseDTO.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.sql.Date;
 
@@ -16,10 +20,15 @@ public class ExpenseDTO {
 
     private String expenseId;
 
+    @NotBlank(message = "Expanse name cannot be empty")
+    @Size(min=3, message = "Expanse name should be atleast {min} characters")
     private String name;
+
 
     private String description;
 
+    @NotNull(message="Expanse amount cannot be empty!")
+    @Min(value = 1, message = "Expense amount can not be less than 1")
     private BigDecimal amount;
 
     private Date date;
