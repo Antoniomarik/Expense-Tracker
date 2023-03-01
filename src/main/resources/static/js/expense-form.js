c3bcab1cc36a9c3f6a1f7e430a5ecd40c0c9a1ed
Antoniomarik <am46906@oss.unist.hr>
2023-03-01 23:56:42 +0100

    added client side validation on the app(for name)

--- src/main/resources/static/js/expense-form.js
+++ src/main/resources/static/js/expense-form.js
@@ -5,4 +5,23 @@ $(function(){
     changeYear: true,
     maxDate: new Date()
     });
+
+    const $expenseForm = $("#expenseform");
+
+    if($expenseForm.length){
+    $expenseForm.validate({
+        rules:{
+            name:{
+                required:true,
+                minlength: 3
+            }
+        },
+        messages:{
+            name:{
+                required: 'Please enter expanse name',
+                minlength: 'Expanse ame shpould be minimum 3'
+                }
+
+            }
+    })}
 })
