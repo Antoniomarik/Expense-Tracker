664cc6077abc083198a05166584a35da8894a313
Antoniomarik <am46906@oss.unist.hr>
2023-03-09 15:40:14 +0100

    modified csc, development moved to main branch, features will be used only for additional features

--- src/main/java/makelli/expensetracker/Config/CustomSecurityConfig.java
+++ src/main/java/makelli/expensetracker/Config/CustomSecurityConfig.java
@@ -1,10 +1,20 @@
 package makelli.expensetracker.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/","/login","/register")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
 }
